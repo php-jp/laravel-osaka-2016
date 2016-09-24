3f7abc7ffb7a2cb12474f579f637ad943862573e
sshono <s.shosho1210@gmail.com>
2016-09-24 14:36:13 +0900

    追加修正

--- frontend/assets/data/sessions.js
+++ frontend/assets/data/sessions.js
@@ -1,6 +1,7 @@
 module.exports = [
     {
         name: "後藤知宏",
+        kana: "Goto Tomohiro",
         at: "株式会社 chatbox",
         link: "http://chatbox-inc.com/page/member/mikakane/",
         photo: "mikakane.jpg",
@@ -8,13 +9,23 @@ module.exports = [
         title: "Laravel アプリケーションとテスト"
     },
     {
-        name: "松尾 大(まつお まさる)",
+        name: "松尾 大",
+        kana: "Matsuo Masaru",
         at: "株式会社イノベーター・ジャパン",
         link: "https://twitter.com/localdisk",
         photo: "mikakane.jpg",
         bio: "PHPとJavaが好きなエンジニア。Laravelに困ってそうな人がいたら答えるのが趣味。Laravel養成読本とLaravelリファレンスを書きました。",
         title: "Laravelにおける後悔しないためのアプリケーション設計",
         description: "これまでに「○○という処理を作りたいのだけどLaravelではどうするのが正しいのか？」という質問を多く受けました。\n本セッションではその疑問に答えるべく私がLaravelを使ったアプリケーションを作成する際に気をつけていること、心がけていることを発表したいと思います。\n",
+    },
+    {
+        name: "ショウノシオリ",
+        kana: "Shono Shiori",
+        at: "株式会社 chatbox",
+        link: "http://chatbox-inc.com/page/member/shono/",
+        photo: "SHOSHO.jpg",
+        bio: "Laravelを使ったり、フロントエンドを触ったりしているエンジニアです。",
+        title: "Laravel5.3 の新機能",
     },
 
 
