b43bb081ec6902b0effb3459498633078ec9dc36
mikakane <mikakane2@gmail.com>
2016-09-25 13:31:51 +0900

    プロフィールなど追加

--- frontend/assets/data/sessions.js
+++ frontend/assets/data/sessions.js
@@ -1,19 +1,10 @@
 module.exports = [
     {
-        name: "後藤知宏",
-        kana: "Goto Tomohiro",
-        at: "株式会社 chatbox",
-        link: "http://chatbox-inc.com/page/member/mikakane/",
-        photo: "mikakane.jpg",
-        bio: "関西フロントエンドUGとかやってます。",
-        title: "Laravel アプリケーションとテスト"
-    },
-    {
         name: "松尾 大",
         kana: "Matsuo Masaru",
         at: "株式会社イノベーター・ジャパン",
         link: "https://twitter.com/localdisk",
-        photo: "mikakane.jpg",
+        photo: "default.jpg",
         bio: "PHPとJavaが好きなエンジニア。Laravelに困ってそうな人がいたら答えるのが趣味。Laravel養成読本とLaravelリファレンスを書きました。",
         title: "Laravelにおける後悔しないためのアプリケーション設計",
         description: "これまでに「○○という処理を作りたいのだけどLaravelではどうするのが正しいのか？」という質問を多く受けました。\n本セッションではその疑問に答えるべく私がLaravelを使ったアプリケーションを作成する際に気をつけていること、心がけていることを発表したいと思います。\n",
@@ -23,11 +14,17 @@ module.exports = [
         kana: "Shono Shiori",
         at: "株式会社 chatbox",
         link: "http://chatbox-inc.com/page/member/shono/",
-        photo: "SHOSHO.jpg",
+        photo: "shono.jpg",
         bio: "Laravelを使ったり、フロントエンドを触ったりしているエンジニアです。",
         title: "Laravel5.3 の新機能",
     },
-
-
-
+    {
+        name: "後藤知宏",
+        kana: "Goto Tomohiro",
+        at: "株式会社 chatbox",
+        link: "http://chatbox-inc.com/page/member/mikakane/",
+        photo: "mikakane.jpg",
+        bio: "関西フロントエンドUGとかやってます。",
+        title: "Laravel アプリケーションとテスト"
+    },
 ];
