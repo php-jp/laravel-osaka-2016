042f65c9b675558aa5d64021b257159061d56cd6
mikakane <mikakane2@gmail.com>
2016-10-08 14:01:14 +0900

    update

--- frontend/assets/data/sessions.js
+++ frontend/assets/data/sessions.js
@@ -1,5 +1,14 @@
 module.exports = [
     {
+        name: "ショウノシオリ",
+        kana: "Shono Shiori",
+        at: "株式会社 chatbox",
+        link: "http://chatbox-inc.com/page/member/shono/",
+        photo: "shono.jpg",
+        bio: "Laravelを使ったり、フロントエンドを触ったりしているエンジニアです。",
+        title: "Laravel5.3 の新機能",
+    },
+    {
         name: "松尾 大",
         kana: "Matsuo Masaru",
         at: "株式会社イノベーター・ジャパン",
@@ -10,13 +19,21 @@ module.exports = [
         description: "これまでに「○○という処理を作りたいのだけどLaravelではどうするのが正しいのか？」という質問を多く受けました。\n本セッションではその疑問に答えるべく私がLaravelを使ったアプリケーションを作成する際に気をつけていること、心がけていることを発表したいと思います。\n",
     },
     {
-        name: "ショウノシオリ",
-        kana: "Shono Shiori",
-        at: "株式会社 chatbox",
-        link: "http://chatbox-inc.com/page/member/shono/",
-        photo: "shono.jpg",
-        bio: "Laravelを使ったり、フロントエンドを触ったりしているエンジニアです。",
-        title: "Laravel5.3 の新機能",
+        name: "大村 創太郎",
+        link: "http://omoon.org/",
+        photo: "omoon.jpeg",
+        bio: " PHPを使ったWEBアプリケーションの開発・運用をやっています。",
+        title: "Laravel でアプリケーションを開発し Heroku で公開するまでの一部始終",
+        description:"Laravel 標準の開発環境「Homestead（あるいは Valet）」を使って小さなアプリケーションを開発し、それを Heroku で公開するまでの一部始終をデモを交えながらお話します。",
+    },
+    {
+        name: "新原 雅司",
+        at: "１✕１株式会社",
+        link: "http://blog.shin1x1.com/",
+        photo: "shin1.png",
+        bio: " PHP をメインに Web システムの開発、プロジェクトへの技術サポートなどを行っている。",
+        title: "DDDパターンを活用したLaravelアプリケーション開発",
+        description:"Laravelは、自由度の高いフレームワークです。数多く有用な機能がコンポーネントとして提供されていますが、それらをどのように組み合わせてデザイン（設計）するかはフレームワーク利用者に委ねられています。\n本セッションでは、レイヤードアーキテクチャを中心に、実際のLaravelアプリケーション開発で活用したDDDパターンについてお話します。",
     },
     {
         name: "増永 玲",
@@ -27,7 +44,7 @@ module.exports = [
         description:"Laravel の Authorization 機能で「誰が」「何に対して」「何を」やっていいかを簡単に実装できるようになりました。ユーザーに権限を与えたり禁止したりするコードを実際に書きながら、アクセスコントロールをより楽ちんに行う方法を考えてみます。",
     },
     {
-        name: " 高橋 慎一",
+        name: "高橋 慎一",
         at: "cloudpack",
         link: "https://cloudpack.jp",
         photo: "tkp.jpg",
@@ -36,15 +53,7 @@ module.exports = [
         description:"Vue.js on S3 & Lumen on EC2 & ユーザ認証 on CognitoUserPoolを使った、「疎結合」で「コード量を少なく」なおかつ「保守性の高い」プロダクトを作った経験と、その際の思考・技術的選択、メリット・デメリットについてお話します。",
     },
     {
-        name: " 大村 創太郎",
-        link: "http://omoon.org/",
-        photo: "omoon.jpeg",
-        bio: " PHPを使ったWEBアプリケーションの開発・運用をやっています。",
-        title: "Laravel でアプリケーションを開発し Heroku で公開するまでの一部始終",
-        description:"Laravel 標準の開発環境「Homestead（あるいは Valet）」を使って小さなアプリケーションを開発し、それを Heroku で公開するまでの一部始終をデモを交えながらお話します。",
-    },
-    {
-        name: "後藤知宏",
+        name: "後藤 知宏",
         kana: "Goto Tomohiro",
         at: "株式会社 chatbox",
         link: "http://chatbox-inc.com/page/member/mikakane/",
@@ -52,4 +61,6 @@ module.exports = [
         bio: "関西フロントエンドUGとかやってます。",
         title: "Laravel アプリケーションとテスト"
     },
+    {
+    }
 ];
